0646c2168d79989e64eb48c9d759ad65fda7f86c
4501104220 <thangtrantt@hotmail.com>
2021-11-06 13:27:11 +0700

    update: flutter - helper: a

---
+++ ebicycle_rental/lib/helper/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart';
+
+class Constant{
+  // Khởi tạo tên MethodChannel để bên java bắt sự kiện
+  static const platform = MethodChannel("Transaction");
+  // Thông tin chủ thẻ
+  static const codeCard = "team_lluminate";
+  static const owner = "Team Illuminate";
+  static const cvvCode = 206;
+  static const dateExpired = "1231";
+}
