e063228a50e2a5d51fd2e0fd78ab4cb189c44803
4501104220 <thangtrantt@hotmail.com>
2021-11-09 16:11:58 +0700

    fix: main act and add: app.apk 2

--- ebicycle_rental/lib/helper/constant.dart
+++ ebicycle_rental/lib/helper/constant.dart
@@ -4,7 +4,7 @@ class Constant{
   // Khởi tạo tên MethodChannel để bên java bắt sự kiện
   static const platform = MethodChannel("Transaction");
   // Thông tin chủ thẻ
-  static const codeCard = "team_lluminate";
+  static const codeCard = "team_illuminate";
   static const owner = "Team Illuminate";
   static const cvvCode = 206;
   static const dateExpired = "1231";
